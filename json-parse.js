64d080d35792e19088b17a686e7542077a308e6c
Arlin Schaffel <arlin.schaffel@saskpolytech.ca>
2023-01-10 13:55:45 -0600

    Adding json-parse.js - added code from exercise - JSON string with wnba player data

---
+++ json-parse.js
@@ -0,0 +1,32 @@
+const wnba_player_data = `{
+    "players": [
+        {
+            "name": "A'ja Wilson",
+            "team": "Las Vegas Aces",
+            "points_per_game": 20.5,
+            "rebounds_per_game": 8.5,
+            "assists_per_game": 2.0
+        },
+        {
+            "name": "Breanna Stewart",
+            "team": "Seattle Storm",
+            "points_per_game": 18.0,
+            "rebounds_per_game": 8.8,
+            "assists_per_game": 3.4
+        },
+        {
+            "name": "Nneka Ogwumike",
+            "team": "Los Angeles Sparks",
+            "points_per_game": 19.7,
+            "rebounds_per_game": 9.1,
+            "assists_per_game": 1.5
+        },
+        {
+            "name": "Kelsey Plum",
+            "team": "Las Vegas Aces",
+            "points_per_game": 17.5,
+            "rebounds_per_game": 2.8,
+            "assists_per_game": 5.1
+        }
+    ]
+}`;
